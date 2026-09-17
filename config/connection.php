a50f8b0f08b1ad4b3fb9b58019bd6e33f2870d06
delivel <ytdeli924@gmail.com>
2026-09-17 07:38:41 -0700

    Fix syntax errors in connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = getenv("DB_HOST");
-$port = getenv("DB_PORT";
-$username = getenv("DB_USER";
-$password = getenv("DB_PASSWORD";
-$dbname = getenv("DB_NAME";
+$port = getenv("DB_PORT");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$dbname = getenv("DB_NAME");
 ?>
